dcd10ac890021cbaf105b59376993afe06f3e2a4
Sergey Nazarov <s.nazarov@mixvel.com>
2026-02-21 15:09:34 +0300

    адаптеры

---
+++ src/FGS.Adapters/JsonConvert/Lobby/LobbyEventJsonConvert.cs
@@ -0,0 +1,11 @@
+﻿using FGS.Domain.FgsLobby.Events;
+using FGS.Domain.Services;
+
+namespace FGS.Adapters.JsonConvert.Lobby;
+
+public class LobbyEventJsonConvert : ILobbyEventJsonConvert
+{
+    public byte[] Serialize(LobbyEvent e) => e.Accept(LobbyEventJsonSerializeToByteArrayVisitor.Instance);
+
+    public string SerializeToString(LobbyEvent e) => e.Accept(LobbyEventJsonSerializeToStringVisitor.Instance);
+}
