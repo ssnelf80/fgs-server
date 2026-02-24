cbb2874c2d602adbd7bc05b11b7cec4b377cb66c
Sergey Nazarov <s.nazarov@mixvel.com>
2026-02-24 13:58:33 +0300

    миграции

---
+++ src/FGS.DAL/Context/Migrations/20260224103145_Initial.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FGS.DAL.Context.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ConnectionTracker",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    LobbyId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PlayerRole = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConnectionTracker", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EventSourceStreamTracker",
+                columns: table => new
+                {
+                    StreamTypeId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CommitPosition = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    PreparePosition = table.Column<decimal>(type: "numeric(20,0)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventSourceStreamTracker", x => x.StreamTypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lobby",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    MasterUserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    PlayersCount = table.Column<long>(type: "bigint", nullable: false),
+                    ConnectedUsers = table.Column<Guid[]>(type: "uuid[]", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lobby", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConnectionTracker_LobbyId",
+                table: "ConnectionTracker",
+                column: "LobbyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ConnectionTracker");
+
+            migrationBuilder.DropTable(
+                name: "EventSourceStreamTracker");
+
+            migrationBuilder.DropTable(
+                name: "Lobby");
+        }
+    }
+}
